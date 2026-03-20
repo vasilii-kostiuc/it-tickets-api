969d309508a9ff75dafb4f3b7c0555e6f18d1354
vasilii-kostiuc <vasilii.costiuc@proton.me>
2026-03-20 11:31:20 +0200

    refactor: Remove ClientsService and update CORS allowed origins configuration

--- config/cors.php
+++ config/cors.php
@@ -1,15 +1,12 @@
 <?php
 
-
 return [
 
     'paths' => ['api/*', 'broadcasting/auth', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    //'allowed_origins' => ['*'],
-    'allowed_origins' => ['http://localhost:5176'], // Vite dev server
-
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
